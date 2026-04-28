8f1bd775979bf756aba069aa229a77726a311793
Ќазими Фејми <fejmi.kjazimi@students.finki.ukim.mk>
2026-04-28 16:44:22 +0200

    V.2 of Lab3 to exercise Background Services and ETL. Finished Background Deletion for deleting Attendances with Absent status and Consultation Session held longer than before 7 days ago.

---
+++ Laboratory-Exercise-3-v2/Service/Jobs/DeleteAttendanceBackgroundService.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Service.Interface;
+
+namespace Service.Jobs;
+
+public class DeleteAttendanceBackgroundService : BackgroundService
+{
+    private readonly IServiceScopeFactory _scopeFactory;
+    private readonly ILogger<DeleteAttendanceBackgroundService> _logger;
+
+    public DeleteAttendanceBackgroundService(IServiceScopeFactory scopeFactory,
+        ILogger<DeleteAttendanceBackgroundService> logger)
+    {
+        _scopeFactory = scopeFactory;
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation("Deletion of Attendances service starting...");
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            try
+            {
+                using var scope = _scopeFactory.CreateScope();
+
+                var service = scope.ServiceProvider.GetRequiredService<IAttendanceService>();
+
+                var attendances = await service.GetNotConfirmedAndOlderThan7Days();
+                
+                _logger.LogInformation($"Total {attendances.Count} attendances were fetched.");
+                
+                if (attendances.Count > 0)
+                {
+                    foreach (var attendance in attendances)
+                    {
+                        await service.DeleteByIdAsync(attendance.Id);
+                        _logger.LogInformation($"Attendance with id {attendance.Id} was deleted.");
+                    }
+                }
+
+            }
+            catch (Exception ex) when (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Deletion of attendances failed.");
+            }
+            _logger.LogInformation("Deletion of Attendances job finished.");
+            await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
+        }
+    }
+}
